5d33146c298063b1e9495957f7a2e13313fe677d
marimonto <marimontod@gmail.com>
2021-04-02 22:46:04 -0500

    Primeros pasos con Angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TitleComponent } from './title/title.component';
+import { FullScreenComponent } from './full-screen/full-screen.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TitleComponent,
+    FullScreenComponent
   ],
   imports: [
     BrowserModule,
